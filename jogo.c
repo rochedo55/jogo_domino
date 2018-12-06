cadf00baa36cf9b7a4f6b243f5e24eb5c9210c21
rochedo55 <43376647+rochedo55@users.noreply.github.com>
2018-12-06 10:48:28 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -3,11 +3,40 @@
 
 int main(){
 
-	int x=0; 
-	float v1[5] = {1/x};
-	for (int i=x;i<6;i++){
-		v1[i]={1+(i/10)};
+	double x=0;
+	/* a ideia é representar cada peça de um dominó como um numero decimal
+	como por exemplo a peca |1|2| que sera representada pelo numero decimal 1,2
+	com isso podemos pegar cada lado do |1|2| pegar_o_2 = ((int)peca - peca)*10;
+	pegar_o_1 = (int)peca;     
+	*/ 
+	//definindo as pecas
+	double v1[5] = {1/x};
+	for (double i=x;i<6;i++){
+		v1[i]=(1+(i/10));
 	}
+	double v2[5] = {2/x};
+	for (double i=x;i<6;i++){
+		v2[i]=(2+(i/10));
+	}
+	double v3[5] = {3/x};
+	for (double i=x;i<6;i++){
+		v3[i]=(3+(i/10));
+	}
+	double v4[5] = {4/x};
+	for (double i=x;i<6;i++){
+		v4[i]=(4+(i/10));
+	}
+	double v5[5] = {5/x};
+	for (double i=x;i<6;i++){
+		v5[i]=(5+(i/10));
+	}
+	double v6[5] = {6/x};
+	for (double i=x;i<6;i++){
+		v6[i]=(6+(i/10));
+	}
+	/*for (int i=0;i<6;i++){
+		printf("%f\n",v1[i] );
+	}*/
 
 
 
